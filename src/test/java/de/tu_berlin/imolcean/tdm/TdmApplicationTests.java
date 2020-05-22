747e11d7abc7a25a4aa9e1120a89bce5f4eba14c
Igor Molcean <molcean@campus.tu-berlin.de>
2020-05-22 12:22:03 +0300

    Let's get it started!

---
+++ src/test/java/de/tu_berlin/imolcean/tdm/TdmApplicationTests.java
@@ -0,0 +1,11 @@
+package de.tu_berlin.imolcean.tdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdmApplicationTests
+{
+    @Test
+    void contextLoads() {}
+}
