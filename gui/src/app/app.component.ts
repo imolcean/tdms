b44223604f5b1ca10cfdef1a58815ba22b12404d
Igor Molcean <molcean@campus.tu-berlin.de>
2020-06-24 11:32:12 +0300

    Menu added

--- gui/src/app/app.component.ts
+++ gui/src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public items: MenuItem[];
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: "File",
+        items: [
+          {label: 'New', icon: 'pi pi-plus'},
+          {label: 'Open', icon: 'pi pi-download'},
+          {label: 'Settings', icon: 'pi pi-cog'}
+        ]
+      },
+      {
+        label: "Edit",
+        items: [
+          {label: 'Undo', icon: 'pi pi-replay'},
+          {label: 'Redo', icon: 'pi pi-refresh'}
+        ]
+      },
+      {
+        label: "Tools",
+        items: [
+          {label: 'Connections', icon: 'pi pi-sitemap'},
+          {label: 'Plugins', icon: 'pi pi-th-large'}
+        ]
+      }
+    ];
+  }
 }
--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
