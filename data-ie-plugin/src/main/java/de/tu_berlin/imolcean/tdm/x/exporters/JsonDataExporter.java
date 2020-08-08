31f884a0ba79902155d104f1cb281c07f605ca0b
Igor Molcean <molcean@campus.tu-berlin.de>
2020-08-08 11:03:49 +0300

    JsonDataExporter exports data to the provided file system path

--- data-ie-plugin/src/main/java/de/tu_berlin/imolcean/tdm/x/exporters/JsonDataExporter.java
+++ data-ie-plugin/src/main/java/de/tu_berlin/imolcean/tdm/x/exporters/JsonDataExporter.java
@@ -1,16 +1,22 @@
 package de.tu_berlin.imolcean.tdm.x.exporters;
 
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.tu_berlin.imolcean.tdm.api.dto.TableContentDto;
 import de.tu_berlin.imolcean.tdm.api.interfaces.exporter.DataExporter;
 import de.tu_berlin.imolcean.tdm.api.services.SchemaService;
 import de.tu_berlin.imolcean.tdm.api.services.TableContentService;
 import lombok.extern.java.Log;
+import org.apache.commons.io.FileUtils;
 import org.pf4j.Extension;
 import schemacrawler.schema.NamedObject;
 import schemacrawler.schema.Table;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -21,17 +27,29 @@ import java.util.stream.Collectors;
 @Log
 public class JsonDataExporter implements DataExporter
 {
-//    private final Path exportDir;
+    private final Path exportDir;
 
     private SchemaService schemaService;
     private TableContentService tableContentService;
 
-//    public JsonDataExporter(Properties properties)
-//    {
-//        this.exportDir = Paths.get(properties.getProperty("export.path"));
-//
-//        log.fine("Export directory: " + exportDir.toString());
-//    }
+    public JsonDataExporter(Properties properties) throws IOException
+    {
+        this.exportDir = Paths.get(properties.getProperty("export.path"));
+
+        try
+        {
+            Files.createDirectory(this.exportDir);
+        }
+        catch(FileAlreadyExistsException e)
+        {
+            if(!Files.isDirectory(this.exportDir))
+            {
+                throw new IllegalArgumentException("Provided export dir exists and is not a directory");
+            }
+        }
+
+        log.fine("Export directory: " + exportDir.toString());
+    }
 
     // TODO Insert SchemaService through DI
     @Override
@@ -44,10 +62,18 @@ public class JsonDataExporter implements DataExporter
     @Override
     public void exportData(DataSource ds) throws Exception
     {
+        log.info("Exporting data");
+
         ObjectMapper mapper = new ObjectMapper();
+        mapper.setDefaultPrettyPrinter(
+                new DefaultPrettyPrinter().withArrayIndenter(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE));
+
+        FileUtils.cleanDirectory(this.exportDir.toFile());
 
         for(String tableName : schemaService.getOccupiedTableNames(ds))
         {
+            log.fine("Exporting " + tableName);
+
             Table table = schemaService.getTable(ds, tableName);
 
             List<String> columnNames = table.getColumns().stream()
@@ -57,8 +83,13 @@ public class JsonDataExporter implements DataExporter
 
             TableContentDto dto = new TableContentDto(tableName, columnNames, rows);
 
-            String json = mapper.writeValueAsString(dto);
-            System.out.println(json);
+            Path file = Files.createFile(Paths.get(exportDir.toAbsolutePath().toString(), tableName + ".json"));
+
+            mapper.writerWithDefaultPrettyPrinter().writeValue(file.toFile(), dto);
+
+            log.fine(String.format("Export of %s finished", tableName));
         }
+
+        log.info("Data export finished");
     }
 }
